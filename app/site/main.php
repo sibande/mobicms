aae2de9d6ba755f4b8f8d5a676afb96badd4d198
Jose Sibande <jbsibande@gmail.com>
2011-06-23 16:13:49 +0200

    21st century mobicms port

---
+++ app/site/main.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main site controller
+ *
+ * @package     mobicms
+ * @subpackage  site
+ * @author      JB Sibande
+ */
+
+class Site_Main extends Fuuze
+{
+  public function index()
+  {
+    $this->render('index.html');
+  }
+
+
+
+
+
+}
---
+++ data/pgsql.sql
@@ -0,0 +1,107 @@
+CREATE TABLE users (
+    id bigserial UNIQUE,
+    username varchar(60),
+    password varchar(500) NOT NULL,
+    email varchar(100),
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id, username, email)
+);
+
+CREATE TABLE profile (
+    id bigserial,
+    uid bigint NOT NULL REFERENCES users (id),
+    birthdate date,
+    sex varchar(1),
+    country varchar(100),
+    city varchar(100),
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE uconnection (
+    id bigserial UNIQUE,
+    user_one_id bigint REFERENCES users (id),
+    user_two_id bigint REFERENCES users (id),
+    is_blocked bool DEFAULT FALSE,
+    ucon_id bigint,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE message (
+    id bigserial,
+    sender_id bigint REFERENCES users (id),
+    receiver_id bigint REFERENCES users (id),
+    exchange_id bigint REFERENCES uconnection (id),
+    status smallint,
+    is_read bool DEFAULT FALSE,
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE contact (
+    id bigserial,
+    uid bigint REFERENCES users (id),
+    full_name varchar(100),
+    email varchar(100),
+    message text NOT NULL,
+    net_id text,
+    is_read bool NOT NULL DEFAULT FALSE,
+    is_active bool NOT NULL DEFAULT TRUE,
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE content_type (
+    id bigserial UNIQUE,
+    label varchar(100) NOT NULL,
+    table_id varchar(100),
+    is_active bool NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (id, label)
+);
+
+CREATE TABLE content_group (
+    id bigserial UNIQUE,
+    typeid int REFERENCES content_type (id),
+    label varchar(500) NOT NULL,
+    description varchar(1000),
+    is_active bool NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE chat (
+    id bigserial,
+    uid bigint NOT NULL REFERENCES users (id),
+    message text NOT NULL,
+    room_id int NOT NULL REFERENCES content_group (id),
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE discussion (
+    id bigserial,
+    uid bigint NOT NULL REFERENCES users (id),
+    topic varchar(500) NOT NULL,
+    body text,
+    groupid int REFERENCES content_group (id),
+    is_active bool NOT NULL DEFAULT TRUE,
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE comments (
+    id bigserial,
+    uid bigint REFERENCES users (id),
+    body text,
+    item_id bigint NOT NULL,
+    typeid int NOT NULL REFERENCES content_type (id),
+    is_active bool NOT NULL DEFAULT TRUE,
+    udatetime timestamp NOT NULL,
+    datetime timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
