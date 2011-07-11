f2a32cb08fdb26cc1a01dbe1ec221d188a038f92
Jose Sibande <jbsibande@gmail.com>
2011-07-11 15:36:06 +0200

    Homepage featured users.

--- app/site/main.php
+++ app/site/main.php
@@ -11,6 +11,13 @@ class Site_Main extends FController
 {
   public function index($request)
   {
+    $featured = $this->db->query('SELECT u.username, u.datetime, p.profile_pic FROM users u '.
+				 'LEFT OUTER JOIN profile p ON (p.uid=u.id) '.
+				 'WHERE p.profile_pic IS NOT NULL ORDER BY u.datetime DESC LIMIT 2');
+    
+    $featured = $featured->fetchAll();
+
+    $this->data['featured'] = $featured;
     $this->data['request'] = $request;
     $this->render('index.html', $this->data);
   }
